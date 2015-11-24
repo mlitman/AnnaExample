00228c92910d2555d42f125fd879213898586d4d
Mike Litman <awesomefat@gmail.com>
2015-11-24 09:08:05 -0600

    Updated for 3rd type of tableview using a drop in view controller placeholder

--- AnnaExample/TableViewExampleTVC.swift
+++ AnnaExample/TableViewExampleTVC.swift
@@ -8,105 +8,26 @@
 
 import UIKit
 
-class TableViewExampleTVC: UIViewController, UITableViewDelegate, UITableViewDataSource
+class TableViewExampleTVC: UIViewController
 {
-    @IBOutlet weak var tv1: UITableView!
-    @IBOutlet weak var tv2: UITableView!
-    var theValues = [String]()
+    
+    @IBOutlet weak var tvPlaceholder: UIView!
+    var theTableViewController : SupportTVC!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        self.theTableViewController = self.storyboard?.instantiateViewControllerWithIdentifier("SupportTVC") as! SupportTVC
+        self.tvPlaceholder.addSubview(self.theTableViewController.view)
 
-        for(var i = 0; i < 100; i++)
-        {
-            self.theValues.append("Row \(i)")
-        }
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int
-    {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        if(tableView == tv1)
-        {
-            return self.theValues.count
-        }
-        else
-        {
-            return 5
-        }
-    }
-
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-    {
-        let cell = tableView.dequeueReusableCellWithIdentifier("elephant", forIndexPath: indexPath)
-
-        // Configure the cell...
-        if(tableView == tv1)
-        {
-            cell.textLabel!.text = self.theValues[indexPath.row]
-        }
-        else
-        {
-            cell.textLabel!.text = "blah"
-        }
-        return cell
-    }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
